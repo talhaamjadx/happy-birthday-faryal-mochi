477a8edb46080f8ccab61a665c35e28c41f7cfcc
talhaamjadx <talhaamjadx@live.com>
2021-03-25 00:42:29 +0500

    grgre

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+// const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 
 module.exports = {
     configureWebpack: {
         entry: "./src/components/decorations.vue",
         plugins:[
-            new HtmlWebpackPlugin(),
+            // new HtmlWebpackPlugin(),
             new PreloadWebpackPlugin({
                 rel: 'preload',
                 as: 'script'
